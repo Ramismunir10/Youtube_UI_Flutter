c8a345c51e26b7922bccb4b6c079a632ebc319ea
ramismunir10 <ramismunir10@gmail.com>
2024-02-29 17:00:31 +0500

    YOUTUBE UI

--- lib/main.dart
+++ lib/main.dart
@@ -56,6 +56,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: Image.asset(
